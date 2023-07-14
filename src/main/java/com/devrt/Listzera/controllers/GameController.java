822a3cf5ae3e592010c801bfb682f9aae9dab942
tiago <tigdrt@gmail.com>
2023-07-14 15:21:50 -0300

    findbyid method

--- src/main/java/com/devrt/Listzera/controllers/GameController.java
+++ src/main/java/com/devrt/Listzera/controllers/GameController.java
@@ -1,9 +1,12 @@
 package com.devrt.Listzera.controllers;
 
+import com.devrt.Listzera.dto.GameDTO;
 import com.devrt.Listzera.dto.GameMinDTO;
+import com.devrt.Listzera.entities.Game;
 import com.devrt.Listzera.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +18,11 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
     @GetMapping
     public List<GameMinDTO> findAll() {
         return gameService.findAll();
--- src/main/java/com/devrt/Listzera/services/GameService.java
+++ src/main/java/com/devrt/Listzera/services/GameService.java
@@ -1,11 +1,12 @@
 package com.devrt.Listzera.services;
 
+import com.devrt.Listzera.dto.GameDTO;
 import com.devrt.Listzera.dto.GameMinDTO;
 import com.devrt.Listzera.entities.Game;
 import com.devrt.Listzera.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {  // this findById returns an optional type, to get that id we need to use .get();
+        Game result = gameRepository.findById(id).get(); // implementar tratamento de excecao caso nao exista o id
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true) // to maintain ACID
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
